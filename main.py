9bb3c5779e3abeef43e369066a7f00552a3f601e
git-songsh <86220586+git-songsh@users.noreply.github.com>
2023-09-02 22:26:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ st.write("---")
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
+    
+    # 메뉴 선택을 위한 사이드바
+    selected_option = st.selectbox('선택할 기기를 바라보세요', ['TV', '가습기', '에어컨'])
 
-# 메뉴 선택을 위한 사이드바
-selected_option = st.sidebar.selectbox('선택할 기기를 바라보세요', ['TV', '가습기', '에어컨'])
 
 # 사용자가 선택한 옵션에 따라 다른 콘텐츠 표시
 if selected_option == 'TV':
