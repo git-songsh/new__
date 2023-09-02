2e6ae825b02432bbcb02fbad3ecb3f2ee624c546
git-songsh <86220586+git-songsh@users.noreply.github.com>
2023-09-02 22:59:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if selected_option == 'TV':
     tv_question = st.text_input('TV에게 질문을 입력하세요')
     
     if st.button('TV에게 질문하기', key='tv_button'):
-        with st.spinner('Wait for it...'):
+        with st.spinner('Wait for it...')
 
 
 elif selected_option == '가습기':
