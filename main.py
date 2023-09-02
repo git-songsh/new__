cbf53fd6556b3fa2cbcc8dcdf972b9be26c87e78
git-songsh <86220586+git-songsh@users.noreply.github.com>
2023-09-02 22:21:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
 
 # 메뉴 선택을 위한 사이드바
-selected_option = st.sidebar.selectbox('선택할 기기를 바라보세요', ['TV', '가습기', '페이지 3'])
+selected_option = st.sidebar.selectbox('선택할 기기를 바라보세요', ['TV', '가습기', '에어컨'])
 
 # 사용자가 선택한 옵션에 따라 다른 콘텐츠 표시
-if selected_option == '페이지 1':
-    st.title('페이지 1')
+if selected_option == 'TV':
+    st.title('TV')
     # 페이지 1의 내용을 표시
-elif selected_option == '페이지 2':
-    st.title('페이지 2')
+
+elif selected_option == '가습기':
+    st.title('가습기')
     # 페이지 2의 내용을 표시
-elif selected_option == '페이지 3':
-    st.title('페이지 3')
+
+elif selected_option == '에어컨':
+    st.title('에어컨')
     # 페이지 3의 내용을 표시
