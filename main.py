8ad4ed6e9f157287280aa8bd826280917c7e60f7
git-songsh <86220586+git-songsh@users.noreply.github.com>
2023-09-02 23:39:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ room_img = room_img.resize((650, int(650 * (room_img.height / room_img.width))))
 st.image(room_img, width=650)
 
 # 메뉴 선택
-selected_option = st.selectbox('선택할 기기를 바라보세요!', ['기기 선택', 'TV', '가습기', '에어컨'])
+selected_option = st.selectbox('선택할 기기를 바라보세요!', ['기기 선택', 'TV를 바라본다', '가습기를 바라본다', '에어컨을 바라본다'])
 
 
 # 사용자가 선택한 옵션에 따라 다른 콘텐츠 표시
 if selected_option == '기기 선택':
     st.write(" ")
     
-elif selected_option == 'TV':
+elif selected_option == 'TV를 바라본다':
     tv_img = Image.open('person_TV.jpg')
     tv_img = tv_img.resize((100, 100))
     st.image(tv_img)
@@ -40,7 +40,7 @@ elif selected_option == 'TV':
     
     st.button('TV에게 질문하기', key='tv_button')
 
-elif selected_option == '가습기':
+elif selected_option == '가습기를 바라본다':
     hm_img = Image.open('person_HM.jpg')
     hm_img = hm_img.resize((100, 100))
     st.image(hm_img)
@@ -52,7 +52,7 @@ elif selected_option == '가습기':
     
     st.button('가습기에게 질문하기', key='hm_button')
 
-elif selected_option == '에어컨':
+elif selected_option == '에어컨을 바라본다':
     ac_img = Image.open('person_AC.jpg')
     ac_img = ac_img.resize((100, 100))
     st.image(ac_img)
