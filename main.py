45331ec3ce461549fee138d81af640ed2eb16014
git-songsh <86220586+git-songsh@users.noreply.github.com>
2023-09-02 23:41:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ elif selected_option == 'TV를 바라본다':
     tv_img = tv_img.resize((100, 100))
     st.image(tv_img)
     
-    st.success('당신은 TV를 바라보고 선택하였습니다!')
+    st.success('당신은 TV를 선택하였습니다!')
     st.header('TV :sunglasses:',divider='rainbow')
 
     tv_question = st.text_input('TV에게 질문을 입력하세요!')
@@ -45,7 +45,7 @@ elif selected_option == '가습기를 바라본다':
     hm_img = hm_img.resize((100, 100))
     st.image(hm_img)
     
-    st.success('당신은 가습기를 바라보고 선택하였습니다!')
+    st.success('당신은 가습기를 선택하였습니다!')
     st.header('가습기 :sunglasses:',divider='rainbow')
 
     hm_question = st.text_input('가습기에게 질문을 입력하세요!')
@@ -57,7 +57,7 @@ elif selected_option == '에어컨을 바라본다':
     ac_img = ac_img.resize((100, 100))
     st.image(ac_img)
     
-    st.success('당신은 에어컨을 바라보고 선택하였습니다!')
+    st.success('당신은 에어컨을 선택하였습니다!')
     st.header('에어컨 :sunglasses:',divider='rainbow')
 
     ac_question = st.text_input('에어컨에게 질문을 입력하세요!')
